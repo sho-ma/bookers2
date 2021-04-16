dc93ffc7d201f46f18783baf6d16b29ad524a8c9
sho-ma <shomaegawa510@gmail.com>
2021-04-16 15:25:37 +0000

    [Add]Bootstrapを使うために

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,17 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
   end
 
   def index
+    @books = Book.all
   end
 
   def show
@@ -13,4 +19,9 @@ class BooksController < ApplicationController
 
   def destroy
   end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :opinion)
 end
