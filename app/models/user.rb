440e16fe1771d61ee0577e48b7116199a9120dda
sho-ma <shomaegawa510@gmail.com>
2021-05-03 03:49:01 +0000

    [Add]コメント機能&いいね機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   attachment :profile_image
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates:name, presence: true, uniqueness: true, length: {in: 2..20} 
   validates:introduction, length: {maximum:50} 
