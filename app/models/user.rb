21f8d63f7fa72f574693a552e53175bec3d7c6b8
sho-ma <shomaegawa510@gmail.com>
2021-05-05 15:01:38 +0000

    [Add]検索機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,20 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
+  
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
 
   validates:name, presence: true, uniqueness: true, length: {in: 2..20} 
   validates:introduction, length: {maximum:50} 
